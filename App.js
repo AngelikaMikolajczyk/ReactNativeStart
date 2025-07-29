30151d63ba9dd644180d5781153d50435fc484db
Angelika <angelika.mikolajczyk11@gmail.com>
2025-07-29 11:57:33 +0200

    styling

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View>
+        <Text>Some Text</Text>
+      </View>
+      <Text style={{margin: 20, borderWidth: 2, borderColor: 'red', padding: 16}}>Another Text</Text>
+      <Button title='Click me'/>
     </View>
   );
 }
